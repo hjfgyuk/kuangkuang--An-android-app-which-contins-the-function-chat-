c014d666d00ee236ce3ff3c2e1ee0202632038ee
turtle <2860048531@qq.com>
2025-08-21 10:40:55 +0800

    完成用户的注册

---
+++ src/test/java/com/kuangkuang/kuangkuang/KuangkuangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuangkuang.kuangkuang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuangkuangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
